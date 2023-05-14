e652de0a9e46a5c5d4a5a6c49fe313d1de024d3c
nakahara-c <ttcocoan@gmail.com>
2023-05-15 03:18:51 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,232 @@
+/*** 
+ * Copyright (c) 2021 Whitefox
+ * This code is part of RomanTypeParser and is released under the MIT License.
+ * https://github.com/WhiteFox-Lugh/RomanTypeParser/blob/main/LICENSE
+***/
+
+
+'use strict';
+
+// This function is based on code from RomanTypeParser by Whitefox (MIT License)
+import { parser } from './parser.js';
+
+let word = document.getElementById('word');
+let wordRoman = document.getElementById('wordRoman');
+const OKButton = document.getElementById('OK');
+const STOPButton = document.getElementById('STOP');
+
+
+
+const timer = document.getElementById('timer');
+const count = document.getElementById('count');
+const kpm = document.getElementById('kpm');
+
+let wordMem = '';
+
+let timerArray = [];
+let typeText;
+
+let timeLabel = [];
+let kpmArray = [];
+let chart = null;
+
+let roman = '';
+let tmpNum = 0;
+
+
+word.addEventListener('input', () => {
+    parseWord();
+});
+
+async function parseWord() {
+    await (async () => typeText = await parser(word.value))();
+    roman = typeText.judgeAutomaton.map(elem => elem[0]).join('');
+    wordRoman.value = roman;
+}
+
+parseWord();
+
+OKButton.addEventListener('click', () => {
+    window.addEventListener('keydown', judgeKeys, false);
+    word.disabled = true;
+    OKButton.disabled = true;
+    STOPButton.disabled = false;
+    wordRoman.disabled = true;
+    wordMem = word.value;
+    timeLabel = [];
+    kpmArray = [];
+});
+
+STOPButton.addEventListener('click', () => {
+    window.removeEventListener('keydown', judgeKeys, false);
+    word.disabled = false;
+    OKButton.disabled = false;
+    STOPButton.disabled = true;
+    wordRoman.disabled = false;
+    stopInterval();
+    word.value = wordMem;
+    parseWord();
+
+});
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        STOPButton.click();
+    }
+    if (e.key === 'Enter' || e.key === ' ') {
+        OKButton.click();
+    }
+}, false);
+
+
+
+
+function firstKeyPressed() {
+    timer.textContent = "0.0";
+    count.textContent = "0";
+    kpm.textContent = "0";
+    timerArray.push(setInterval(startTimer, 100));
+    tmpNum = typeText.judgeAutomaton[0].length;
+}
+
+function startTimer() {
+    let currentTime = Number(timer.textContent) + 0.1;
+    currentTime = Number.parseFloat(currentTime).toFixed(1);
+    timer.textContent = currentTime;
+    if (currentTime > 0.2) {
+        kpmArray.push(Number(kpm.textContent));
+        timeLabel.push(currentTime);
+    }
+}
+
+function stopInterval() {
+    if (timerArray.length > 0) {
+        clearInterval(timerArray.shift());
+    }
+}
+
+function judgeKeys(e) {
+
+    //e.preventDefault();
+    let typedKey = e.key;
+
+    //judgeAutomaton受け取ってそれに応じて判定していく
+    /*
+    japaneseWord.parsedSentence -> ['た'], ['ぷ'],...
+    japaneseWord.judgeAutomaton -> ['ta'], ['pu'],...
+    */
+
+    let currentHiraganaLength = typeText.parsedSentence[0].length;
+    let currentRoman = typeText.judgeAutomaton[0];
+
+    let isOK = false;
+    let isLast = false;
+
+    for (let i = 0; i < currentRoman.length; i++) {
+        if (typedKey === currentRoman[i][0]) {
+            isOK = true;
+
+            if (currentRoman[i].length === 1) {
+                isLast = true;
+            } else {
+                currentRoman[i] = currentRoman[i].slice(1);
+            }
+        }
+    }
+
+    if (isOK) {
+        if (timerArray.length === 0) {
+            firstKeyPressed();
+        }
+
+        //console.log(tmpNum);
+        tmpNum -= 1;
+        if (tmpNum >= 0) wordRoman.value = wordRoman.value.slice(1);
+
+
+
+        if (isLast) {
+            typeText.parsedSentence.shift();
+            typeText.judgeAutomaton.shift();
+            word.value = word.value.slice(currentHiraganaLength);
+            if (typeText.judgeAutomaton[0]) {
+                tmpNum = typeText.judgeAutomaton[0][0].length;
+            }
+            
+        }
+
+        count.textContent = String(Number(count.textContent) + 1);
+        let kpmValue = Math.round(Number(count.textContent) / Number(timer.textContent) * 60);
+        if (kpmValue === Infinity) kpmValue = 0;
+        kpm.textContent = String(kpmValue);
+
+    }
+    if (wordRoman.value === '' || !typeText.judgeAutomaton[0]) {
+        typeFinish(true);
+    }
+
+}
+
+
+function typeFinish(isCompleted) {
+    stopInterval();
+    window.removeEventListener('keydown', judgeKeys, false);
+    drawChart();
+
+}
+
+function initializeDataBox () {
+    timer.textContent = '0.000';
+    count.textContent = '0';
+    kpm.textContent = '0';
+    return;
+}
+
+
+function drawChart() {
+
+    const ctx = document.getElementById('chart').getContext('2d');
+
+    const chartData = kpmArray;
+
+    if (chart) chart.destroy();
+    
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: timeLabel,
+            datasets: [{
+                label: 'KPM',
+                data: chartData,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1,
+                pointStyle: 'none',
+                pointBackgroundColor: 'rgba(255, 99, 132, 1)',
+                pointBorderColor: 'rgba(255, 99, 132, 1)',
+                pointBorderWidth: 2,
+                pointRadius: 4
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                },
+                x: {}
+            },
+            plugins: {
+                tooltip: {
+                    titleColor: 'rgba(0, 0, 0, 1)',
+                    bodyColor: 'rgba(0, 0, 0, 1)',
+
+                    backgroundColor: 'rgba(248, 236, 243, 1)',
+
+                    displayColors: false,
+
+                }
+            }
+        }
+    });
+
+}
---
+++ js/parser.js
@@ -0,0 +1,68 @@
+//にほんごろーまじにする
+
+export async function parser(word) {
+    const mappingDict = await loadMappingDict();
+    let dict = new Object();
+    for (let i = 0; i < mappingDict.length; i++) {
+        let p = mappingDict[i].Pattern;
+        let t = mappingDict[i].TypePattern;
+        dict[p] = t;
+    }
+
+    const hiraToKana = text => {
+        return text.replace(/[\u30A2-\u30F3]/g,
+            m => String.fromCharCode(m.charCodeAt(0) - 96)
+        );
+    };
+    
+    let hiragana = hiraToKana(word);
+    let res = constructTypeSentence(dict, hiragana);
+
+    return res;
+
+}
+
+async function loadMappingDict() {
+    try {
+        const response = await fetch('./js/romanTypingParseDictionary.json');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const mappingDict = await response.json();
+        return mappingDict;
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+}
+
+function constructTypeSentence(mappingDict, sentenceHiragana) {
+    let idx = 0;
+    let uni, bi, tri;
+    let judge = [];
+    let parsedStr = [];
+    while (idx < sentenceHiragana.length) {
+        let validTypeList;
+        uni = sentenceHiragana[idx].toString();
+        bi = (idx + 1 < sentenceHiragana.length) ? sentenceHiragana.substring(idx, idx + 2) : "";
+        tri = (idx + 2 < sentenceHiragana.length) ? sentenceHiragana.substring(idx, idx + 3) : "";
+
+        if (mappingDict.hasOwnProperty(tri)) {
+            validTypeList = mappingDict[tri].slice();
+            idx += 3;
+            parsedStr.push(tri);
+        }
+        else if (mappingDict.hasOwnProperty(bi)) {
+            validTypeList = mappingDict[bi].slice();
+            idx += 2;
+            parsedStr.push(bi);
+        }
+        else {
+            validTypeList = mappingDict[uni].slice();
+            idx += 1;
+            parsedStr.push(uni);
+        }
+        judge.push(validTypeList);
+    }
+
+    return { parsedSentence: parsedStr, judgeAutomaton: judge };
+}
