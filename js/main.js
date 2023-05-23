807470e63b75aa2a6abb1ac679b0fb8253a06053
nakahara-c <ttcocoan@gmail.com>
2023-05-23 23:22:42 +0900

    higgsino導入　いったん打てるようになった

--- js/main.js
+++ js/main.js
@@ -7,11 +7,53 @@
 
 'use strict';
 
+//import { Word } from 'higgsino';
 // This function is based on code from RomanTypeParser by Whitefox (MIT License)
 import { parser } from './parser.js';
 
-let word = document.getElementById('word');
-let wordRoman = document.getElementById('wordRoman');
+/*
+let testWord = new Word('漢字', 'いっちょうめ');
+
+const viewConsole = () => {
+    // Example
+    console.log("例文を取得", testWord.example);
+
+    // Kana
+    console.log("よみがなの全てを取得", testWord.kana.all);
+    console.log("よみがなの打った部分を取得", testWord.kana.typed);
+    console.log("よみがなの打ってない部分を取得", testWord.kana.untyped);
+
+    // Roman
+    console.log("ローマ字の全文字を取得", testWord.roman.all);
+    console.log("ローマ字の打った文字を取得", testWord.roman.typed);
+    console.log("ローマ字の打ってない文字を取得", testWord.roman.untyped);
+
+    console.log("ローマ字の全配列を取得", testWord.roman.array.all);
+    console.log("ローマ字の打った配列を取得", testWord.roman.array.typed);
+    console.log("ローマ字の打ってない配列を取得", testWord.roman.array.typed);
+}
+
+viewConsole();
+
+testWord.typed('i');
+
+viewConsole();
+
+testWord.typed('x');
+
+viewConsole();
+
+testWord.typed('t');
+testWord.typed('u');
+testWord.typed('c');
+
+viewConsole();
+*/
+
+
+let wordHiraganaArea = document.getElementById('word');
+let wordRomanArea = document.getElementById('wordRoman');
+let word = '';
 const OKButton = document.getElementById('OK');
 const STOPButton = document.getElementById('STOP');
 
@@ -38,25 +80,31 @@ let tmpNum = 0;
 let isPlaying = false;
 
 
-word.addEventListener('input', () => {
+wordHiraganaArea.addEventListener('input', () => {
     parseWord();
 });
 
-async function parseWord() {
-    await (async () => typeText = await parser(word.value))();
+async function parseWordOld() {
+    await (async () => typeText = await parser(wordHiraganaArea.value))();
     roman = typeText.judgeAutomaton.map(elem => elem[0]).join('');
-    wordRoman.value = roman;
+    wordRomanArea.value = roman;
+}
+
+function parseWord () {
+    word = new Word('', wordHiraganaArea.value);
+    console.log(word);
+    wordRomanArea.value = word.roman.all;
 }
 
 parseWord();
 
 OKButton.addEventListener('click', () => {
     window.addEventListener('keydown', judgeKeys, false);
-    word.disabled = true;
+    wordHiraganaArea.disabled = true;
     OKButton.disabled = true;
     STOPButton.disabled = false;
-    wordRoman.disabled = true;
-    wordMem = word.value;
+    wordRomanArea.disabled = true;
+    wordMem = wordHiraganaArea.value;
     timeLabel = [];
     kpmArray = [];
 });
@@ -64,12 +112,12 @@ OKButton.addEventListener('click', () => {
 STOPButton.addEventListener('click', () => {
     window.removeEventListener('keydown', judgeKeys, false);
     isPlaying = false;
-    word.disabled = false;
+    wordHiraganaArea.disabled = false;
     OKButton.disabled = false;
     STOPButton.disabled = true;
-    wordRoman.disabled = false;
+    wordRomanArea.disabled = false;
     clearInterval(intervalId);
-    word.value = wordMem;
+    wordHiraganaArea.value = wordMem;
     parseWord();
 
 });
@@ -92,7 +140,7 @@ function firstKeyPressed() {
     kpm.textContent = "0";
 
     startTime = performance.now();
-    intervalId = setInterval(startTimer, 100);
+    intervalId = setInterval(startTimer, 1);
 
     tmpNum = typeText.judgeAutomaton[0].length;
 }
@@ -108,8 +156,26 @@ function startTimer() {
     }
 }
 
-
 function judgeKeys(e) {
+    let typedKey = e.key;
+
+    const [isMiss, isFinish] = word.typed(typedKey);
+    if (isFinish) typeFinish(true);
+    if (isMiss);
+
+    console.log(word.kana.typed);
+
+    //ひらがなの方に反映
+    wordHiraganaArea.value = word.kana.untyped;
+
+    //ローマ字の方に反映
+    wordRomanArea.value = word.roman.untyped;
+
+}
+
+
+
+function judgeKeysOld(e) {
 
     //e.preventDefault();
     let typedKey = e.key;
@@ -146,12 +212,12 @@ function judgeKeys(e) {
 
         //console.log(tmpNum);
         tmpNum -= 1;
-        if (tmpNum >= 0) wordRoman.value = wordRoman.value.slice(1);
+        if (tmpNum >= 0) wordRomanArea.value = wordRomanArea.value.slice(1);
 
         if (isLast) {
             typeText.parsedSentence.shift();
             typeText.judgeAutomaton.shift();
-            word.value = word.value.slice(currentHiraganaLength);
+            wordHiraganaArea.value = wordHiraganaArea.value.slice(currentHiraganaLength);
             if (typeText.judgeAutomaton[0]) {
                 tmpNum = typeText.judgeAutomaton[0][0].length;
             }
@@ -164,7 +230,7 @@ function judgeKeys(e) {
         kpm.textContent = String(kpmValue);
 
     }
-    if (wordRoman.value === '' || !typeText.judgeAutomaton[0]) {
+    if (wordRomanArea.value === '' || !typeText.judgeAutomaton[0]) {
         typeFinish(true);
     }
 
